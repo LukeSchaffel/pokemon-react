4d677190fe9f949c01c90ce0322f27ec7481228b
LukeSchaffel <lukeschaffel@gmail.com>
2022-07-13 11:24:10 -0400

    link

--- src/pages/MoveDetails/MoveDetails.jsx
+++ src/pages/MoveDetails/MoveDetails.jsx
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { getMoveInfo } from "../../components/services/poke-api";
 
 
 const MoveDetails = () => {
   const location = useLocation()
-  console.log(location);
   const URL = location.state.move.move.url
   const [move, setMove] = useState({})
   const [loading, setLoading] = useState(true)
+  const [pokemonList, setPokemonList] = useState([])
 
   const getAndSetMoveInfo = async (URL) => {
     const newMoveInfo = await getMoveInfo(URL)
     setMove(newMoveInfo);
+    setPokemonList(newMoveInfo.learned_by_pokemon)
     setLoading(false)
   }
 
@@ -22,6 +23,8 @@ const MoveDetails = () => {
     getAndSetMoveInfo(URL)
   }, [])
 
+
+
   if (loading) {
     return <h1>Loading</h1>
   } else {
@@ -29,8 +32,26 @@ const MoveDetails = () => {
     return (
       <article className="info-div">
         <header>
-          <h1>{move.name}</h1>
+          <h3>{move.name}</h3>
+          <h3>{move.type.name}</h3>
         </header>
+        <div>
+          Learned By:
+          <ul>
+          {pokemonList.map((pokemon, i) => {
+            const { name, url } = pokemon
+            return (
+            <li>
+              <Link to='/pokemon-details'>
+                {name}
+              </Link>
+            </li>
+            )
+          })
+
+          }
+          </ul>
+        </div>
 
       </article>
     );
