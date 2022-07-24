186b804ae6019a199f6050915f11cff3fffa9888
LukeSchaffel <lukeschaffel@gmail.com>
2022-07-24 11:04:56 -0400

    colors on info page

--- src/pages/PokemonDetails/PokemonDetail.jsx
+++ src/pages/PokemonDetails/PokemonDetail.jsx
@@ -4,14 +4,15 @@ import PhotoGalery from "../../components/PhotoGalery/PhotoGalery";
 import MoveList from "../../components/MoveList/MoveList";
 import EncounterLocations from "../../components/EncounterLocations/EncounterLocations";
 import Abilities from "../../components/Abilities/Abilities";
+import colors from "../../components/services/color-data";
 
 const PokemonDetails = ({ loading, setLoading }) => {
   const location = useLocation()
   const [pokemon, setPokemon] = useState(location.state.singlePokemon)
-
+  const type = pokemon.types[0].type.name
 
   return (
-    <article className="info-div">
+    <article className="info-div" style={{backgroundColor:colors[type]}}>
       <header>
         <h1>{pokemon.name}</h1>
         <h1># {pokemon.id}</h1>
