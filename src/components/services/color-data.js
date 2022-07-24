454f3f4cc9f03df66394d0094d18c094b0fb4572
LukeSchaffel <lukeschaffel@gmail.com>
2022-07-24 10:59:45 -0400

    move colors

---
+++ src/components/services/color-data.js
@@ -0,0 +1,22 @@
+const colors = {
+  bug:'rgb(136,151,14)',
+  dark:'rgb(54,45,32, .75)',
+  dragon:'rgb(104,87,195)',
+  electric:'rgb(251,183,24)',
+  fairy:'rgb(241,173,243)',
+  fighting:'rgb(126,49,29, .75)',
+  fire: 'rgb(239,101,63)',
+  flying: 'rgb(136,152,227)',
+  ghost: 'rgb(96,97,177, .75)',
+  grass: 'rgb(136,199,81)',
+  ground: 'rgb(212,181,91)',
+  ice: 'rgb(140,218,242)',
+  normal: 'rgb(212,210,205)',
+  poison: 'rgb(163,103,166)',
+  pyscic: 'rgb(235,71,128)',
+  rock: 'rgb(183,158,84)',
+  steel: 'rgb(182,183,194)',
+  water: 'rgb(67,158,245, .75)'
+}
+
+export default colors
